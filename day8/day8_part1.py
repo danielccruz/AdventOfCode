2919d079e880a15bec4320b81bdcb268a48cf58e
danielccruz <daniel_ccruz@outlook.com>
2020-12-08 18:10:25 +0000

    day5 done.

---
+++ day8/day8_part1.py
@@ -0,0 +1,28 @@
+def processor(instructions, position, accumulator, visited_positions):
+    print(position)
+    if position in visited_positions:
+        print('zzz')
+        return accumulator
+    if position > len(instructions):
+        print('xxx')
+        return accumulator
+    if instructions[position][0] == 'jmp':
+        visited_positions.append(position)
+        position = position + int(instructions[position][1])
+        return processor(instructions, position, accumulator, visited_positions)
+    if instructions[position][0] == 'nop':
+        visited_positions.append(position)
+        return processor(instructions, position + 1, accumulator, visited_positions)
+    if instructions[position][0] == 'acc':
+        visited_positions.append(position)
+        accumulator = accumulator + int(instructions[position][1])
+        return processor(instructions, position + 1, accumulator, visited_positions)
+
+
+instructions = []
+with open('input_file.txt') as input_file:
+    for line in input_file.readlines():
+        line = line.strip().split(' ')
+        instructions.append(line)
+
+print(processor(instructions, 0, 0, []))
---
+++ day8/day8_part2.py
@@ -0,0 +1,50 @@
+def processor(instructions, position, accumulator, visited_positions):
+    if position in visited_positions:
+        return -1
+    if position > len(instructions):
+        return accumulator
+    if instructions[position][0] == 'jmp':
+        visited_positions.append(position)
+        position = position + int(instructions[position][1])
+        return processor(instructions, position, accumulator, visited_positions)
+    if instructions[position][0] == 'nop':
+        visited_positions.append(position)
+        return processor(instructions, position + 1, accumulator, visited_positions)
+    if instructions[position][0] == 'acc':
+        visited_positions.append(position)
+        accumulator = accumulator + int(instructions[position][1])
+        return processor(instructions, position + 1, accumulator, visited_positions)
+    return accumulator
+
+
+instructions = []
+with open('input_file.txt') as input_file:
+    for line in input_file.readlines():
+        line = line.strip().split(' ')
+        instructions.append(line)
+
+
+###########################
+BRUTE_FORCE_SOLUTION = True
+###########################
+
+
+for instruction_i in range(0, len(instructions)):
+    result = processor(instructions, 0, 0, [])
+    if result == -1:
+        if instructions[instruction_i][0] == 'nop':
+            instructions[instruction_i][0] = 'jmp'
+            result = processor(instructions, 0, 0, [])
+            if result == -1:
+                instructions[instruction_i][0] = 'nop'
+            else:
+                print('Bug found on position: {}. Accumulator value is {}.'.format(instruction_i, result))
+                break
+        elif instructions[instruction_i][0] == 'jmp':
+            instructions[instruction_i][0] = 'nop'
+            result = processor(instructions, 0, 0, [])
+            if result == -1:
+                instructions[instruction_i][0] = 'jmp'
+            else:
+                print('Bug found on position: {}. Accumulator value is {}.'.format(instruction_i, result))
+                break
